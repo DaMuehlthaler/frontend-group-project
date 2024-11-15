ca293904a74e2cbbccab2bc0d715b2c1d2fd1771
DaMuehlthaler <muehlthaler.daniel@johak.at>
2024-11-15 11:08:03 +0100

    daisyui add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="text-red-600">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+<button class="btn">Daisy UI</button>
