d625ffd983a500de283f1cfa63dd1b2192d44d3e
Lukassssssssssss <ober.lukas@johak.at>
2024-11-19 11:12:15 +0100

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p class="text-red-600">
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	Penis <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 </p>
 <button class="btn">Daisy UI</button>
