82a5da17e5b1840a525531f2f2f13dd84a9c5449
Lukassssssssssss <ober.lukas@johak.at>
2024-11-19 11:35:28 +0100

    heading push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-600">
-	Penis <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
+<h1 class="font-extrabold text-yellow-400">Wichtiger Text</h1>
+
 <button class="btn">Daisy UI</button>
