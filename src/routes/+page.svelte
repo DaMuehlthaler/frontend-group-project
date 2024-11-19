7d6b23669b6b2c8d4e88d7f7b443a5ddfa0e3d66
DaMuehlthaler <muehlthaler.daniel@johak.at>
2024-11-19 12:19:09 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-600">
-	Penis <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
-<button class="btn">Daisy UI</button>
+<div class="h-screen w-screen bg-white">
+	<img src="/images/bg-main-desktop.png" alt="ihhh" class="h-screen -z-10" />
+	<form class="inline" action="/">
+		<input
+			type="text"
+			placeholder="e.g. Hansi Hinterseer"
+			class="input input-bordered w-full max-w-xs"
+		/>
+		<br />
+		<input type="number" placeholder="1234 5678" class="input input-bordered w-full max-w-xs" />
+		<br />
+		<input type="number" placeholder="MM" class="input input-bordered w-full max-w-xs" />
+
+		<input type="Number" placeholder="YY" class="input input-bordered w-full max-w-xs" />
+
+		<input type="number" placeholder="e.g.123" class="input input-bordered w-full max-w-xs" />
+	</form>
+	<br />
+	<button class="btn glass">Submit</button>
+</div>
